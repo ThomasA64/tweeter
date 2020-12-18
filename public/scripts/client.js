d6620f7cfaa02e1b1d4f5c6484381367239548ea
ThomasA64 <thomas.arthur64@gmail.com>
2020-12-18 00:36:34 +0000

    commented and cleaned up code before submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,27 +1,24 @@
-
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready(function() {
+$(document).ready(function () {
+  const renderTweets = function (tweets) {
+    console.log(tweets);
+    for (const tweet of tweets) {
+      const newTweet = createTweetElement(tweet);
 
-const renderTweets = function(tweets) {
-  console.log(tweets)
-  for (const tweet of tweets) {
-    const newTweet = createTweetElement(tweet)
-    
-    $(".tweetContainer").append(newTweet)
-  }
-// loops through tweets
-// calls createTweetElement for each tweet
-// takes return value and appends it to the tweets container
-};
+      $(".tweetContainer").append(newTweet);
+    }
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+  };
 
-const createTweetElement = function(data) {
-  let created_at = new Date(data.created_at);
-  let $tweet = 
-    `<article class="tweet">
+  const createTweetElement = function (data) {
+    let created_at = new Date(data.created_at);
+    let $tweet = `<article class="tweet">
       <header class="artTweets"> 
       <div class="user">  
       <figure>
@@ -46,66 +43,58 @@ const createTweetElement = function(data) {
       <i class="fas fa-heart"></i>
       </div>
       </footer>
-  </article>`
-return $tweet;
-};
+  </article>`;
+    return $tweet;
+  };
 
-// ${created_at.toLocaleDateString()} at ${created_at.toLocaleTimeString()}
+  // Form submittion using JQuery:
+  const loadTweets = function () {
+    $.get("/tweets/").then(function (tweets) {
+      renderTweets(tweets);
+    });
+  };
 
-
-// Form submittion using JQuery:
-const loadTweets =  function () {
-  $.get("/tweets/").then (function (tweets) {
-    renderTweets(tweets)
-  });
-}
-
-const showError = (msg) => {
-  let $error = `
+  const showError = (msg) => {
+    let $error = `
    
       ${msg}
   
   `;
-  $('.error').append($error).slidedown()
-} 
+    $(".error").append($error).slidedown();
+  };
 
+  $("#showForm").click(() => {
+    $(".new-tweet").toggleClass("visible");
+  });
+  // this is to toggle the new-tweet form on the click on a button.
 
-$('#showForm').click(() => {
-  $('.new-tweet').toggleClass('visible');
-})
-// this is to toggle the new-tweet form on the click on a button.  
+  $(function () {
+    const tForm = $("#tweetForm");
+    tForm.submit(function (ev) {
+      $(".error").empty();
+      const textArea = tForm.find("textarea");
 
-$(function () {
-  const tForm = $('#tweetForm');
-  tForm.submit(function (ev) {
-    $('.error').empty()
-    const textArea = tForm.find('textarea');
-
-    ev.preventDefault();
-     // If statement to make sure it doesn't submit if over 140 characters or under 1 (empty).
-    if( textArea.val().length > 140) {
-      showError('Your tweet has exceeded 140 characters!')
-    } else if (textArea.val().length === 0) {
-      showError('Your tweet is empty!')
-    }
-      else {
-      $.ajax({
-      type: tForm.attr('method'),
-      url: tForm.attr('action'),
-      data: tForm.serialize(),
-      success: function (data) {
-          //loadTweets();
-          const newTweet = createTweetElement(data);
-          $(".tweetContainer").prepend(newTweet);
-          textArea.val('')      
+      ev.preventDefault();
+      // If statement to make sure it doesn't submit if over 140 characters or under 1 (empty).
+      if (textArea.val().length > 140) {
+        showError("Your tweet has exceeded 140 characters!");
+      } else if (textArea.val().length === 0) {
+        showError("Your tweet is empty!");
+      } else {
+        $.ajax({
+          type: tForm.attr("method"),
+          url: tForm.attr("action"),
+          data: tForm.serialize(),
+          success: function (data) {
+            //loadTweets();
+            const newTweet = createTweetElement(data);
+            $(".tweetContainer").prepend(newTweet);
+            textArea.val("");
+          },
+        });
       }
+    });
   });
-    }
-      
-  });
-});
 
-loadTweets();
-
+  loadTweets();
 }); // document.ready ending curly brace here.
-
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,20 +1,17 @@
-
-$(document).ready(function() {
+$(document).ready(function () {
   // --- our code goes here ---
   const TweetTextArea = document.getElementById("taTweet");
   console.log(TweetTextArea);
   const RemainingTweetChars = document.getElementById("counterChar");
-  
+
   const MaxText = 140;
-  
-  TweetTextArea.addEventListener('keyup', () => {
-      const remainingText = (MaxText - TweetTextArea.value.length);
-  
-      const colour = remainingText <= 0 ? 'red' : null;
-  
-      RemainingTweetChars.textContent = `${remainingText}`
-      RemainingTweetChars.style.color = colour;
-  })
+
+  TweetTextArea.addEventListener("keyup", () => {
+    const remainingText = MaxText - TweetTextArea.value.length;
+
+    const colour = remainingText <= 0 ? "red" : null;
+
+    RemainingTweetChars.textContent = `${remainingText}`;
+    RemainingTweetChars.style.color = colour;
+  });
 });
-
-
